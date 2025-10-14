e0c0b664c23a27523ab67c61cefa5b44ecd9210c
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2025-10-14 10:27:23 -0500

    crea archivos de importancia

---
+++ script.js
@@ -0,0 +1,38 @@
+function mostrarMensaje(){
+    console.log("Holaaaa como estas");
+}
+
+function capturaTexto(){
+    //capturamos el elemento input
+    let input=document.querySelector("#nuevaTarea");
+    if (input.value===""){
+        alert("El input debe contener un texto");
+        return;
+    }
+
+    //capturamos el valor del input
+    let valorInput=input.value;
+
+    //creamos un elemento li
+    let ul=document.querySelector("#listaTareas");
+
+    //creamos un elemento li
+    let li=document.createElement("li");
+
+    //le asignamos como contenido el valor capturado del input
+    li.textContent=valorInput;
+    
+    //creamos el boton eliminar
+    let button=document.createElement("button");
+    button.textContent="Eliminar";
+    li.appendChild(button);
+    button.onclick=function(){
+        ul.removeChild(li);
+    }
+    // le agrego como hijo el li al elemento padre ul
+    ul.appendChild(li);
+    
+
+    document.querySelector("#nuevaTarea");
+    input.value="";
+}
